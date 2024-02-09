bec884aa25b6ced7afc91bf80f1fdf741ff31454
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-08 21:53:02 -0600

    In the root route, save a random number between 1 and 100 and display a form for the user to guess the number

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, session, render_template
+import random
 
 app = Flask(__name__)
 app.secret_key = "8891techa0128"
 
+@app.route("/")
+def index():
+    if "num" not in session:
+        session["num"] = random.randint(1,100)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
